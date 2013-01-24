4d0bc733e34de7c19de6644521911c306df05f29
Vladimir Starkov <matmuchrapna@gmail.com>
2013-01-24 13:14:00 +0600

    rewrited to use custom paths

--- stsync.py
+++ stsync.py
@@ -3,12 +3,14 @@ import sublime_plugin
 from subprocess import call
 
 
+# sublime.packages_path();
+
 class TestCommand(sublime_plugin.ApplicationCommand):
     def __init__(self):
         super(TestCommand, self).__init__()
         #do your stuf here
         # call(["ping", "192.168.1.1"])
-        call(["node", "./Data/Packages/STsync/app.js", "./settings/"])
+        # call(["node", "./Data/Packages/STsync/app.js", "./settings/"])
 
     def run(self, edit):
         pass
