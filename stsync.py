450d8e6492776eaec997b5ff472d40dea77d76b6
Starkov Vladimir <matmuchrapna@gmail.com>
2013-01-25 03:24:55 +0600

    finally work in the background!!!!

--- stsync.py
+++ stsync.py
@@ -9,14 +9,7 @@ import subprocess
 class TestCommand(sublime_plugin.ApplicationCommand):
     def __init__(self):
         super(TestCommand, self).__init__()
-        #do your stuf here
-        # call(["ping", "192.168.1.1"])
-        # call(["node", "app.js", "../User/"], Shell=true)
-        # call(["node", "./app-dev.js"])
-        #
-        # call(["node", "./Data/Packages/STsync/app-dev.js"], Shell=true)
-        # call(["node", "./Data/Packages/STsync/app-dev.js"])
-        subprocess.Popen(["node", "./Data/Packages/STsync/app-dev.js"])
+        subprocess.Popen(["node", "./Data/Packages/STsync/app-dev.js"], shell=True)
 
     def run(self, edit):
         pass
