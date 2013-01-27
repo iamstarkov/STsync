003ab6215084ed3b50aab5364d90bc97c4298ac7
Starkov Vladimir <matmuchrapna@gmail.com>
2013-01-28 01:07:42 +0600

    small fixes

--- stsync.py
+++ stsync.py
@@ -1,20 +1,15 @@
 import sublime
 import sublime_plugin
 import subprocess
-# from subprocess import call
 
 
 class TestCommand(sublime_plugin.ApplicationCommand):
     def __init__(self):
         super(TestCommand, self).__init__()
-        # subprocess.Popen(["node", "./Data/Packages/STsync/app-dev.js"], shell=True)
-        # subprocess.Popen(["node", "./Data/Packages/STsync/app-dev.js"])
         packages_path = sublime.packages_path()
-        relative_path = '\STsync\\app-dev.js'
-        # relative_path = '\STsync\\echo.js'
+        relative_path = '\STsync\\app.js'
         script_path = ''.join([packages_path, relative_path])
-        # D:\Dropbox\programs\ST2\Data\Packages
-        subprocess.Popen(["node", script_path, sublime.packages_path()])
+        subprocess.Popen(["node", script_path, sublime.packages_path()], shell=True)
 
     def run(self, edit):
         pass
