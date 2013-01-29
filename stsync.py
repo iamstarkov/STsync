2799df84e52cceaf85166beecfc4584d1c0e5bb6
Starkov Vladimir <matmuchrapna@gmail.com>
2013-01-30 00:57:19 +0600

    added log for packages path

--- stsync.py
+++ stsync.py
@@ -9,8 +9,10 @@ class TestCommand(sublime_plugin.ApplicationCommand):
         packages_path = sublime.packages_path()
         relative_path = '\STsync\\app.js'
         script_path = ''.join([packages_path, relative_path])
+        print "path: " + packages_path
+
         # subprocess.Popen(["node", script_path, sublime.packages_path()], shell=True)
-        subprocess.Popen(["node", script_path, sublime.packages_path()])
+        subprocess.Popen(["node", script_path, packages_path])
 
     def run(self, edit):
         pass
